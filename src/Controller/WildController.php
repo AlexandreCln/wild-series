3b15a1d94849d6f2e056eb5d6c0c922a392ad7c2
Alexandre Coulon <alex97.coul@gmail.com>
2019-11-19 14:29:57 +0100

    Quest 05 Routes done.

--- src/Controller/WildController.php
+++ src/Controller/WildController.php
@@ -8,15 +8,30 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @Route("/wild", name="wild_")
+ */
 class WildController extends AbstractController
 {
     /**
-     * @Route("/wild", name="wild_index")
+     * @Route("", name="index")
      */
-    public function index() : Response
+    public function index(): Response
     {
         return $this->render('wild/index.html.twig', [
             'website' => 'Wild Séries',
         ]);
+    }
+
+    /**
+     * @Route("/show/{slug}", requirements={"slug"="[a-z0-9-]+"}, name="show")
+     */
+    public function show($slug = null): Response
+    {
+        if (!empty($slug)) {
+            $modifiedSlug = ucwords(str_replace('-', ' ', $slug));
+        }
+
+            return $this->render('wild/show.html.twig', ['slug' => $modifiedSlug ?? "Aucune série sélectionnée, veuillez choisir une série"]);
     }
 }
